3e0e8cf1c3be7427ae2f5f997935172c770c16fe
Pavel Gyurov <pavel.gjurov@gmail.com>
2016-02-21 22:46:43 +0200

    Update DBManager.java
    allowMultiQueries=true

--- src/shop/DBManager/DBManager.java
+++ src/shop/DBManager/DBManager.java
@@ -6,7 +6,7 @@ import java.sql.*;
 public class DBManager {
 
     private static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
-    private static final String DB_URL = "jdbc:mysql://localhost/";
+    private static final String DB_URL = "jdbc:mysql://test/test?allowMultiQueries=true";
     private static final String USER = "root";
     private static final String PASS = "r00tP@ss";
     private static DBManager dbManager;
